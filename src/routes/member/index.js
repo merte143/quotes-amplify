4cabe6aa414780ca226f53617bb751e9ec60565a
merte143 <timo.h.mueller@gmx.de>
2018-10-28 20:07:39 +0100

    setup db, simple r/w operations from member space

--- src/routes/member/index.js
+++ src/routes/member/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Component } from 'react'
-import { Grid, Row, Col } from 'react-bootstrap'
+import { Grid, Row, Col, Button } from 'react-bootstrap'
 import { push } from 'react-router-redux'
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
@@ -9,9 +9,23 @@ import { connect } from 'react-redux'
 import Amplify from 'aws-amplify';
 import aws_exports from '../../aws-exports.js';
 import { withAuthenticator } from 'aws-amplify-react';
+import { Auth } from 'aws-amplify';
+
+// add db access
+import { Storage, API } from 'aws-amplify';
+
 Amplify.configure(aws_exports);
 
 class Member extends Component {
+
+  componentDidMount() {
+    Auth.currentAuthenticatedUser()
+      .then(user => console.log(user))
+      .catch(err => console.log(err))
+
+    let session = Auth.currentSession();
+    console.log(session)
+  }
 
   render() {
     console.log('hö?')
@@ -20,11 +34,39 @@ class Member extends Component {
         <Row>
           <Col col-xs='12'>
             <h1>Wow, you are logged in.</h1>
-            <p>toll.</p>            
+            <p>toll.</p>    
+            <Button onClick={ this.post }>Post</Button>
+            <Button onClick={ this.get }>Get</Button>
+            <Button onClick={ this.list }>List</Button>
           </Col>
         </Row>
       </Grid>
     )
+  }
+
+  post = async () => {
+    console.log('calling api');
+    const options = {
+      body: {
+        id: '1',
+        text: 'test db',
+        author: 'timo'
+      }
+    }
+    const response = await API.post('quoteapi', '/quotes', options);
+    alert(JSON.stringify(response, null, 2));
+  }
+
+  get = async () => {
+    console.log('calling api');
+    const response = await API.get('quoteapi', '/quotes/object/1');
+    alert(JSON.stringify(response, null, 2));
+  }
+
+  list = async () => {
+    console.log('calling api');
+    const response = await API.get('quoteapi', '/quotes/1');
+    alert(JSON.stringify(response, null, 2));
   }
 }
 
